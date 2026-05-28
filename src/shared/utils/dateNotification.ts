4ab2dc3ce2b25f62a37d3dac48f441bc04de98e7
MarcoReL575 <marcoreyes575@icloud.com>
2026-05-27 20:22:05 -0600

    feat(notifications): Se cambia función que obtenía la hora de la notiifcación a español

--- src/shared/utils/dateNotification.ts
+++ src/shared/utils/dateNotification.ts
@@ -1,5 +1,9 @@
 import { formatDistanceToNow } from 'date-fns'
+import { es } from 'date-fns/locale'
 
 export const formatDateNotification = (date: Date) => {
-    return formatDistanceToNow(date);
+    return formatDistanceToNow(date, {
+        addSuffix: true,
+        locale: es
+    });
 }

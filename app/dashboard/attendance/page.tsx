05a03facba1cce055d7689637347fba9c94dab07
MarcoReL575 <marcoreyes575@icloud.com>
2026-06-14 20:10:05 -0600

    feat(attendance): Agregando una condicional en la página de asistencias, para que muestre la página correcta de acuerdo al rol del usuario

--- app/dashboard/attendance/page.tsx
+++ app/dashboard/attendance/page.tsx
@@ -1,14 +1,18 @@
+import PageStudentsAttendance from '@/src/features/attendance/components/PageStudentsAttendance';
 import MisClasesTechaerPage from '@/src/features/teachers/components/MisClasesTechaerPage';
 import { requireAuth } from '@/src/lib/auth-server';
 import Heading from '@/src/shared/components/typography/Heading'
+import { redirect } from 'next/navigation';
 
 export default async function AttendancePage() {
-  const { session } = await requireAuth();    
-
+  const { session } = await requireAuth();   
+  if(!session.user) redirect('/auth/signin');
+  const rol = session.user.role
+  
   return (
     <>
-      <Heading level={2}>Selecciona una de tus clases para pasar lista</Heading>
-      <MisClasesTechaerPage session={session} />
+      { rol === 'maestro' && <MisClasesTechaerPage session={session} /> }
+      { rol === 'estudiante' && <PageStudentsAttendance /> }
     </>
   )
 }
---
+++ src/features/attendance/components/PageStudentsAttendance.tsx
@@ -0,0 +1,9 @@
+import Heading from "@/src/shared/components/typography/Heading";
+
+export default function PageStudentsAttendance() {
+  return ( 
+    <> 
+        <Heading level={2}>En esta sección puedes llevar un control de tus asistencias.</Heading>
+    </>  
+  )
+}

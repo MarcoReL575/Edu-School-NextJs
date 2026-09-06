b855146dd37e27c88a4f134afc421ffcb2ed4adb
MarcoReL575 <marcoreyes575@icloud.com>
2026-09-05 20:14:26 -0600

    feature: Se agrega sección con título para la página de exámenes

--- src/features/examenes/components/ExamenesTutorSection.tsx
+++ src/features/examenes/components/ExamenesTutorSection.tsx
@@ -1,6 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/src/shared/components/ui/tabs";
 import { getTutorChildrenAction } from "@/src/features/parents/actions/parentsActions";
 import StudentExamPage from "./StudentExamPage";
+import Heading from "@/src/shared/components/typography/Heading";
 
 export default async function ExamenesTutorSection() {
     const children = await getTutorChildrenAction();
@@ -14,19 +15,30 @@ export default async function ExamenesTutorSection() {
     }
 
     return (
-        <Tabs defaultValue={children[0].id} className="flex flex-col">
-            <TabsList variant="line" className="mx-auto border rounded-lg border-gray-600 bg-gray-100 p-2">
+        <>
+            <section>
+                <Heading level={1}>Exámenes Asignados</Heading>
+                <p>
+                    {children.length === 0 
+                        ? 'Revisa los exámenes que se le han asignado a tu hijo'
+                        : 'Revisa los exámenes que se le han asignado a tus hijos.'
+                    }
+                </p>
+            </section>
+            <Tabs defaultValue={children[0].id} className="flex flex-col">
+                <TabsList variant="line" className="mx-auto border rounded-lg border-gray-600 bg-gray-100 p-2">
+                    {children.map((child) => (
+                        <TabsTrigger key={child.id} value={child.id}>
+                            {child.name} {child.lastName}
+                        </TabsTrigger>
+                    ))}
+                </TabsList>
                 {children.map((child) => (
-                    <TabsTrigger key={child.id} value={child.id}>
-                        {child.name} {child.lastName}
-                    </TabsTrigger>
+                    <TabsContent key={child.id} value={child.id}>
+                        <StudentExamPage userId={child.user_id} />
+                    </TabsContent>
                 ))}
-            </TabsList>
-            {children.map((child) => (
-                <TabsContent key={child.id} value={child.id}>
-                    <StudentExamPage userId={child.user_id} />
-                </TabsContent>
-            ))}
-        </Tabs>
+            </Tabs>
+        </>
     )
 }
